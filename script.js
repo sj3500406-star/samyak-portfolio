380e090156628780f85839fa958d51d3422b362a
sj3500406-star <sj3500406@gmail.com>
2026-09-19 18:04:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+< !DOCTYPE html >
+    <html>
+        <head>
+            <tittle>Samyak Jain|BCA Student</tittle>
+            </head>
+            <body>
+
+                <h1>Hi,I'm Samyak Jain</h1>
+                <p>I am a BCA student.</p>
+            </body>
+    </html>
